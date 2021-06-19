6037a95839a0c0d19136b1cdc80b070217ba0892
Jayjay8182 <27012591+Jayjay8182@users.noreply.github.com>
2021-06-19 23:28:58 +0100

    Update Website.py

--- Website.py
+++ Website.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 from OpenSSL import SSL
 
-context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
-context.use_privatekey_file('server.key')
-context.use_certificate_file('server.crt')   
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER) 
+context.load_cert_chain('certificate.crt', 'private.key')
 
 comments=[
 {
