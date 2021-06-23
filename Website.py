feda49f78ba80663fc39db10507cd392886fccee
Jayjay8182 <27012591+Jayjay8182@users.noreply.github.com>
2021-06-23 03:57:54 +0100

    Update Website.py

--- Website.py
+++ Website.py
@@ -1,44 +1,23 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-comments = [
-{
-    'title': 'Reliable',
-    'name': 'Donald',
-    'comment': 'Great for managing employees',
-    'date_posted': '16/04/2020'
-},
-{
-    'title': 'Awesome software',
-    'name': 'Macklemore',
-    'comment': 'Really gets the job done',
-    'date_posted': '27/08/2020'
-},
-{
-    'title': 'i am literally floppa',
-    'name': 'floppa',
-    'comment': 'i actual do not care',
-    'date_posted': '02/03/1974'
-}
-]
-
 @app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html', comments=comments)
+@app.route('/Home')
+def Home():
+    return render_template('Home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/Socials')
+def Socials():
+    return render_template('Socials.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+@app.route('/Github')
+def Github():
+    return render_template('Github.html')
 
-@app.route('/download')
-def download():
-    return render_template('download.html')
+@app.route('/Projects')
+def Projects():
+    return render_template('Projects.html')
 
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port='443', ssl_context=('/etc/letsencrypt/live/jayjay8182.tk/fullchain.pem', '/etc/letsencrypt/live/jayjay8182.tk/privkey.pem'))
+    app.run(debug=True)
